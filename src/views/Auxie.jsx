5e9ddb1d8ee959963944318c9ef8c9d0fb5b536b
carlos cornelio <corneliocarlos.a@gmail.com>
2023-11-21 19:28:27 -0600

    Auxie

---
+++ src/views/Auxie.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Auxie = () => {
+  return (
+    <div>Auxie</div>
+    <p>Auxie es una plataforma dedicada a conectar profesionistas de calidad con clientes potenciales en busqueda de un experto.</p>
+    <p>Contamos con funcionalidad para el cliente donde puede buscar profesionistas y filtrar por tipo de trabajo y tambien ordenar por calificaci'on y precio.</p>
+    <p>El Auxie contara con acceso a nuestra dashboard donde podra visualizar sus datos, trabajos pendientes,realizados y solicitados, sus ganancias y las calificaciones que tiene al igual que un chat donde puede hablar con los clientes en tiempo real.</p>
+    <p>Admin Dashboard</p>
+    )
+}
+
+export default Auxie
