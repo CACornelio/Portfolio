ac2f3193a34c33c63e4732c2a339ffe0e718fe91
carlos cornelio <corneliocarlos.a@gmail.com>
2023-11-23 14:40:16 -0600

    auxie

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import './App.css'
 import Home from './views/Home';
 import Projects from './views/Projects';
+import Auxie from './views/Auxie';
 
 function App() {
   
@@ -12,6 +13,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/projects" element={<Projects />}/>
+        <Route path='/projects/auxie' element={<Auxie/>}/>
       
 
       </Routes>
--- src/views/Auxie.jsx
+++ src/views/Auxie.jsx
@@ -1,12 +1,29 @@
 import React from 'react'
+import YouTube from 'react-youtube';
 
 const Auxie = () => {
+  const videoId = 'GTjuUNxgev4';
+
+  const opts = {
+    height: '390',
+    width: '640',
+    justifyContent: 'center',
+    playerVars: {
+      autoplay: 1,
+    },
+  };
   return (
-    <div>Auxie</div>
+    <div>
+      <h1 style={{textAlign:'center',}}>Auxie</h1>
+    <div style={{justifyContent:'center',alignContent:'center',}}>
+    <YouTube videoId={videoId} opts={opts} />
+
+    </div>
     <p>Auxie es una plataforma dedicada a conectar profesionistas de calidad con clientes potenciales en busqueda de un experto.</p>
     <p>Contamos con funcionalidad para el cliente donde puede buscar profesionistas y filtrar por tipo de trabajo y tambien ordenar por calificaci'on y precio.</p>
     <p>El Auxie contara con acceso a nuestra dashboard donde podra visualizar sus datos, trabajos pendientes,realizados y solicitados, sus ganancias y las calificaciones que tiene al igual que un chat donde puede hablar con los clientes en tiempo real.</p>
-    <p>Admin Dashboard</p>
+    <p>Nuestro Admin Dashboard nos permite realizar cambios en las categorias, desactivar cuentas, realizar soporte a clientes, y tambien visualizar nuestros datos como clientes registrados, trabajos realizados totales, ganancias, entre mas.</p>
+    </div>
     )
 }
 
